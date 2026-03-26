e7c97d0b31009299db2bb8ff6a0621d0240a503a
Berrue <aeberruezo@gmail.com>
2026-03-26 11:11:30 -0300

    Mejora en sistema de tempratura, ahora es por dificultad

--- Numerin.py
+++ Numerin.py
@@ -9,10 +9,14 @@ puntuacionGanada = None
 ronda = 0
 intentos = None
 tirada = None
-
+nombreDificultad = None
+termometro = None
+termometroFacil = [12,7,5,2]
+termometroNormal = [30,15,7,5]
+termometroDificil = [50,30,10,8]
 while True:
     print ("--------- Numerin V 0.1 ---------")
-    dificultad= input("Selecciona la dificultad inicial: \n 1.Facil \n 2.Normal \n 3.Dificil")
+    dificultad= input("Selecciona la dificultad inicial: \n 1.Facil \n 2.Normal \n 3.Dificil\n")
     try:
         dificultad = int(dificultad)
     except ValueError:
@@ -20,31 +24,38 @@ while True:
         continue
     if dificultad == 1:
         dificultad = facil
+        nombreDificultad = "facil"
         intentosMaximos = 3
         intentos = intentosMaximos
         puntuacionGanada = 1
         numeroMaestro = random.randint(facil [0], facil [1])
+        termometro = termometroFacil
         break
     elif dificultad == 2:
         dificultad = normal
+        nombreDificultad = "normal"
         intentosMaximos = 5
         intentos = intentosMaximos
         puntuacionGanada = 3
         numeroMaestro = random.randint(normal [0], normal [1])
+        termometro = termometroNormal
         break
     elif dificultad == 3:
         dificultad = dificil
+        nombreDificultad = "dificil"
         intentosMaximos = 10
         intentos = intentosMaximos
         puntuacionGanada = 6
         numeroMaestro = random.randint(dificil [0], dificil [1])
+        termometro = termometroDificil
         break
     else:
         print("Ingrese una dificultad valida: ")
         continue
+print (f'Elegiste la dificultad {nombreDificultad}, ¿estas listo?')
 
-while True:
-    print (f'Elegiste la dificultad, {dificultad}, ¿estas listo?')
+while True: 
+    print (numeroMaestro)
     tirada = input("¿Que numero sera?: ")
     try:
         tirada = int(tirada)
@@ -54,11 +65,11 @@ while True:
     distancia = abs(numeroMaestro - tirada)
     if tirada != numeroMaestro:
         intentos = intentos -1
-        if distancia >= 12:
+        if distancia >= termometro[0]:
             print ("🥶 frio ")
-        elif distancia >= 10 and distancia <12:
+        elif distancia <= termometro[0] and distancia >termometro[1]:
             print ("🌡️ Tibio")
-        elif distancia >= 5 and distancia <10:
+        elif distancia <= termometro[1] and distancia >termometro[2]:
             print ("🔥 Caliente")
         else:
             print ("🌋 Muy caliente")

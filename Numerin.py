de680f4c89eb6ee4d6cbe6f0426b608cafdaa553
Berrue <aeberruezo@gmail.com>
2026-03-25 22:28:48 -0300

    agregado de puntuacion, numero de intentos y loop jugable

--- Numerin.py
+++ Numerin.py
@@ -1,14 +1,14 @@
 import random
 
-numeroMaestro = random.randint(1,100)
+numeroMaestro = None
 facil = [1,15]
 normal = [1,50]
 dificil = [1,100]
-dificultad = None
 puntuacion  = 0
 puntuacionGanada = None
 ronda = 0
 intentos = None
+tirada = None
 
 while True:
     print ("--------- Numerin V 0.1 ---------")
@@ -20,16 +20,57 @@ while True:
         continue
     if dificultad == 1:
         dificultad = facil
-        intentos = 3
+        intentosMaximos = 3
+        intentos = intentosMaximos
         puntuacionGanada = 1
+        numeroMaestro = random.randint(facil [0], facil [1])
+        break
     elif dificultad == 2:
         dificultad = normal
-        intentos = 5
+        intentosMaximos = 5
+        intentos = intentosMaximos
         puntuacionGanada = 3
+        numeroMaestro = random.randint(normal [0], normal [1])
+        break
     elif dificultad == 3:
         dificultad = dificil
-        intentos = 10
+        intentosMaximos = 10
+        intentos = intentosMaximos
         puntuacionGanada = 6
+        numeroMaestro = random.randint(dificil [0], dificil [1])
+        break
     else:
         print("Ingrese una dificultad valida: ")
         continue
+
+while True:
+    print (f'Elegiste la dificultad, {dificultad}, ¿estas listo?')
+    tirada = input("¿Que numero sera?: ")
+    try:
+        tirada = int(tirada)
+    except ValueError:
+        print ("Ingresa un valor valido")
+        continue
+    distancia = abs(numeroMaestro - tirada)
+    if tirada != numeroMaestro:
+        intentos = intentos -1
+        if distancia >= 12:
+            print ("🥶 frio ")
+        elif distancia >= 10 and distancia <12:
+            print ("🌡️ Tibio")
+        elif distancia >= 5 and distancia <10:
+            print ("🔥 Caliente")
+        else:
+            print ("🌋 Muy caliente")
+    else:
+        print ("Acertaste!! felicitaciones!")
+        puntuacion = puntuacion + puntuacionGanada
+        verificar = input(f'Puntuacion acumulada: {puntuacion} \n ¿Queres seguir jugando? : ')
+        if verificar.lower() == "si":
+            intentos = intentosMaximos
+            numeroMaestro = random.randint(dificultad [0], dificultad [1])
+            continue
+    
+    if intentos == 0:
+        print (f'Perdiste! Puntuacion total: {puntuacion}')
+        break
